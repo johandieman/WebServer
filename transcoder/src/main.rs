8d67fd5297c521ad545015ce01c363e0cd758a6e
johandieman <johanvdmerwelus@gmail.com>
2023-06-09 16:23:06 +0200

    docker file initial transcoder working

--- transcoder/src/main.rs
+++ transcoder/src/main.rs
@@ -1,4 +1,4 @@
-
+extern crate ffmpeg_next as ffmpeg;
 
 
 use std::collections::HashMap;
@@ -7,30 +7,30 @@ use reqwest::header::{HeaderValue, HeaderMap};
 mod transcode;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() {
 
     let client = reqwest::Client::new();
     let mut map = HashMap::new();
-    let headers = HeaderMap::new();
+    let mut headers = HeaderMap::new();
 
-    let myFile = transcode::convert(Some("./test.mp4".to_string()),Some("anull".to_string()),Some(3000));
+    let my_file = transcode::convert(Some("./test.mp4".to_string()),Some("anull".to_string()),Some(3000));
     
-    match myFile  {
+    match my_file  {
         Ok(file)=>{
-            map.insert("file", &file);        
+            map.insert("file", file);        
         },
-        Err(err)=> return Err(Box::new(err)),
+        Err(err)=> panic!("Error: {:?}", err),
+        
     }
 
 
     headers.insert("Authorization", HeaderValue::from_static("Bearer "));
     headers.insert("Content-Type", HeaderValue::from_static("multipart/form-data"));
-    map.insert("model", "whisper-1");
+    // map.insert("model", "whisper-1");
 
     let resp = client.post("https://api.openai.com/v1/audio/transcriptions").headers(headers).json(&map).send().await;
 
 
-    println!("{:#?}", resp);
+    // println!("{:#?}", resp);
 
-    Ok(())
 }
